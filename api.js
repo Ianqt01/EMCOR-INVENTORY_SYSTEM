6ea36795d776300abaa437e8ad839c2772175c89
Ianqt01 <ianmarkturno123@gmail.com>
2026-09-01 20:33:42 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // ====== SET THIS AFTER YOU DEPLOY THE APPS SCRIPT WEB APP ======
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbxwrbMArdqyLxMMiXnjDea4RcLpWqJ7r6_uwxMlN8Vajn5Ay7cqcurlpwnnB_HVovCU/exec";
 // e.g. "https://script.google.com/macros/s/AKfycb.../exec"
 
 async function apiCall(action, payload = {}) {
